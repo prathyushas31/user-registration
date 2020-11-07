f9fe3ff90a26826c3169d5c17223065f26d90d92
prathyushas31 <prathyushas1111@gmail.com>
2020-11-07 20:37:59 +0530

    added should clear all the email samples that are provided seperately

--- userregistration.sh
+++ userregistration.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-read -p "enter password:" Password
-Password_Pattern="(?=.*[A-Z])(?=.*[0-9])(?=.*[*.!@$%^&(){}[\]:;<>,.?\/~_+\-=|#]).{8,}"
-if [[ $Password =~ $Password_Pattern ]]
+read -p "enter email:" email
+email_Pattern="^[0-9a-zA-Z]+([_.+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?"
+if [[ $email =~ $email_Pattern ]]
 then
-	echo "valid"
+        echo "valid"
 else
-	echo "invalid"
+        echo "invalid"
 fi
 
 
