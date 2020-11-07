718f43b562f0d087f9988bf8d3b64748eb79641e
prathyushas31 <prathyushas1111@gmail.com>
2020-11-07 20:12:53 +0530

    added password which contains atleast one uppercase

--- userregistration.sh
+++ userregistration.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 read -p "enter password:" Password
-Password_Pattern="^[a-zA-Z]{8,}$"
+Password_Pattern="^(?=.{10,}$)(?=.*[A-Z]).*$"
 if [[ $Password =~ $Password_Pattern ]]
 then
 	echo "valid"
 else
 	echo "invalid"
 fi
-
 
 
 
